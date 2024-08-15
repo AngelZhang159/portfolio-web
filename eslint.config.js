2a6ea63571513f201f35faf1c5d32e58a0cd2fe9
AngelZhang159 <angelzhang159@gmail.com>
2024-08-15 19:57:15 +0200

    Eslint config and others

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,26 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import standard from "eslint-config-standard";
 
 export default [
-  {
-    files: ['**/*.{js,jsx}'],
-    ignores: ['dist'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    extends: [
-      'standard',
-      'plugin:react/recommended',
-      'plugin:react-hooks/recommended'
-    ],
-    plugins: [
-      'react',
-      'react-hooks',
-      'react-refresh'
-    ],
-    rules: {
-      // Add any custom rules here
-    },
-  },
-]
+	{
+		files: ["**/*.{js,mjs,cjs,jsx}"],
+		languageOptions: {
+			globals: globals.browser,
+		},
+		settings: {
+			react: {
+				version: "detect"
+			}
+		},
+		rules: {
+		}
+	},
+	{
+		languageOptions: { globals: globals.browser }
+	},
+	pluginJs.configs.recommended,
+	pluginReact.configs.flat.recommended,
+	standard,
+];
