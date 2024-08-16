1daa0b266c2c3e685b6e562a41bc000aeb282d27
AngelZhang159 <angelzhang159@gmail.com>
2024-08-16 15:21:23 +0200

    Refactor footer component and add WebInfo component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,5 @@
-import { Trans } from "react-i18next";
-import IconButtonLink from "./footer/IconButtonLink";
 import MadeByMe from "./footer/MadeByMe";
+import WebInfo from "./footer/WebInfo";
 
 export default function Footer() {
 	return (
@@ -10,11 +9,7 @@ export default function Footer() {
 
 				<MadeByMe />
 
-				<p>
-					<Trans i18nKey="built-with">
-						Built with <b className="text-primary">React</b> and <b className="text-primary">TailwindCSS</b> using <b className="text-primary">Visual Studio Code</b>, <br /> deployed via <b className="text-primary">GitHub Pages</b>, and served by <b className="text-primary">Cloudflare</b>.
-					</Trans>
-				</p>
+				<WebInfo />
 			</div>
 		</footer>
 	)
--- src/data/contact.js
+++ src/data/contact.js
@@ -1,6 +1,5 @@
 export const contact = [
 	{ name: "mail", link: "mailto:angelzhang159@gmail.com", image: "/icons/gmail.svg" },
 	{ name: "github", link: "https://github.com/AngelZhang159", image: "/icons/github.svg" },
-	{ name: "linkedin", link: "www.linkedin.com/in/angelzhang159", image: "/icons/linkedin.svg" },
-
+	{ name: "linkedin", link: "https://www.linkedin.com/in/angelzhang159", image: "/icons/linkedin.svg" },
 ]
