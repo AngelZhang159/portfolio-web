e1d27c978cc1c8e408a94b9a3ed4e805974f2f9a
AngelZhang159 <angelzhang159@gmail.com>
2024-08-18 01:56:02 +0200

    Add data

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,5 +1,5 @@
 export const contact = [
-	{ name: "mail", link: "mailto:angelzhang159@gmail.com", image: "/icons/gmail.svg" },
+	{ name: "email", link: "mailto:angelzhang159@gmail.com", image: "/icons/gmail.svg" },
 	{ name: "github", link: "https://github.com/AngelZhang159", image: "/icons/github.svg" },
 	{ name: "linkedin", link: "https://www.linkedin.com/in/angelzhang159", image: "/icons/linkedin.svg" },
 ]
---
+++ src/data/jobBadges.js
@@ -0,0 +1,13 @@
+/*<Badge color="" icon={"/icons/vscode.svg"} text={"VS Code"} />
+					<Badge color="" icon={"/icons/android-studio.svg"} text={"Android Studio"} />
+					<Badge color="" icon={"/icons/xcode.svg"} text={"Xcode"} />
+					<Badge color="" icon={"/icons/linux.svg"} text={"Linux"} />
+					<Badge color="" icon={"/icons/kamailio.svg"} text={"Kamailio"} />*/
+
+export const badges = [
+	{ text: "VS Code", icon: "/icons/vscode.svg", color: "#219edf" },
+	{ text: "Android Studio", icon: "/icons/android-studio.svg", color: "#3ddc84" },
+	{ text: "Xcode", icon: "/icons/xcode.svg", color: "#ffffff" },
+	{ text: "Linux", icon: "/icons/linux.svg", color: "#ffcc33" },
+	{ text: "Kamailio", icon: "/icons/kamailio.svg", color: "#ffffff" },
+]
