c24d0719ed36a06114369db20ffb4f77e2bac7da
AngelZhang159 <angelzhang159@gmail.com>
2024-08-18 03:21:54 +0200

    ESLint config reset

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
-import standard from "eslint-config-standard";
+
 
 export default [
-	{
-		files: ["**/*.{js,mjs,cjs,jsx}"],
-		languageOptions: {
-			globals: globals.browser,
-		},
-		settings: {
-			react: {
-				version: "detect"
-			}
-		},
-		rules: {
-		}
-	},
-	{
-		languageOptions: { globals: globals.browser }
-	},
-	pluginJs.configs.recommended,
-	pluginReact.configs.flat.recommended,
-	standard,
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ];
