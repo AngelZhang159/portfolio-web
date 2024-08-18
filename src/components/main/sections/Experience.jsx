61ded78d63fa6755eef92ef1e1419e51751aa3d0
AngelZhang159 <angelzhang159@gmail.com>
2024-08-18 18:10:02 +0200

    Modify text

--- src/components/main/sections/Experience.jsx
+++ src/components/main/sections/Experience.jsx
@@ -13,31 +13,42 @@ export default function Experience() {
 				{t('experience')}
 			</SectionTitle>
 
-			<div className="text-xl max-w-prose">
+			<div className="inline-flex">
+				<div className="flex flex-col items-center">
+					<div className="bg-green-400 w-6 h-6 rounded-full"></div>
+					<div className="w-1 h-4/5 bg-gradient-to-b from-green-200 to-transparent"></div>
+				</div>
 
+				<div className="flex-col flex items-center px-10">
+					<p>April - June 2024</p>
+					<img src="/images/mundo-sms-logo.webp" alt="" className="w-36 pt-10" />
+				</div>
 
-				<p className="text-3xl"><b>{t('mobile-developer-internship')}</b></p>
-				<br />
-				<b>
-					{t('the-company')}
-				</b>
-				<p>
-					{t('company-def')}
-				</p>
-				<br />
-				<b>
-					{t('my-role')}
-				</b>
-				<p>
-					{t('role-def')}
-				</p>
-				<br />
-				<p>
-					{t('role-additionally')}
-				</p>
+				<div className="text-xl max-w-prose">
 
-				<BadgeGroup badges={badges} />
+					<p className="text-4xl"><b>{t('mobile-developer-internship')}</b></p>
+					<p>Comunicaciones Electrónicas Mundo SMS</p>
+					<br />
+					<b>
+						{t('the-company')}
+					</b>
+					<p>
+						{t('company-def')}
+					</p>
+					<br />
+					<b>
+						{t('my-role')}
+					</b>
+					<ul className="list-disc pl-10">
+						<li>{t('first-app')}</li>
+						<li>{t('deployed-app')}</li>
+						<li>{t('deployed-server')}</li>
+					</ul>
+					<br />
 
+					<BadgeGroup badges={badges} />
+
+				</div>
 			</div>
 		</SectionFrame>
 	)
