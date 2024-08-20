7b424b86d7a3df5eb62f344a1dba505d78ac2840
AngelZhang159 <angelzhang159@gmail.com>
2024-08-20 05:16:34 +0200

    Fix title

--- src/components/main/sections/Projects.jsx
+++ src/components/main/sections/Projects.jsx
@@ -3,13 +3,14 @@ import React from 'react'
 import SectionTitle from "../SectionTitle";
 import Card from "../Card";
 import { projects } from '../../../data/projects'
+import { t } from "i18next";
 
 export default function Projects() {
 	return (
 		<SectionFrame id="projects">
 
 			<SectionTitle fromColor="from-yellow-200" toColor="to-orange-500">
-				Projects
+				{t('projects')}
 			</SectionTitle>
 			<div className="w-full grid justify-between lg:grid-cols-2 sm:grid-cols-1 items-center">
 				{projects.map(project => {
